eb7f82fb6ee9f77f6e901dc34b95c6d7550614dc
canonhead <topbasbaha@gmail.com>
2023-04-23 15:10:47 -0400

    changed function names to indicate what they do

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-function myFunction() {
+function buttonClick() {
     var x = document.getElementById("myNavbar");
     if (x.className === "navbar") {
       x.className += " responsive";
       x.style.width = "280px";
+    } else {
+      x.className = "navbar";
+      x.style.width = "50px";
+    }
+  }
+function mainClick() {
+    var x = document.getElementById("myNavbar");
+    if (x.className === "navbar") {
     } else {
       x.className = "navbar";
       x.style.width = "50px";
